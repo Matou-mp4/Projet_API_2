f554cd49f6f83ca0aca91cd4074c7fbb15140505
Matou <antoine.dangreau@condorcet.be>
2024-02-23 11:55:21 +0100

    Ajout de la documentation dans la classe Classe

--- src/Classe.java
+++ src/Classe.java
@@ -1,16 +1,58 @@
 import java.util.ArrayList;
-
+/**
+ * Classe Classe
+ * @Author Dangreau Antoine
+ * @Version 1.0
+ */
 public class Classe {
+    /**
+     * le sigle et la specialité unique de la classe
+     */
     protected String sigle, specialite;
+    /**
+     * l'annee et le nombre de l'élève de la classe
+     */
     protected int annee, nbreEleves;
+    /**
+     * Liste d'informations de la classe
+     * Chaque infos contient :
+     *  - l'enseignant
+     *  - le cours
+     *  - la salle
+     *  - le nombre d'heures
+     */
     protected ArrayList <Infos> listeInfos = new ArrayList<>();
+    /**
+     * Liste des enseignants et du nombre d'heures
+     */
     protected ArrayList <EnseignantEtHeure> listeEnseigantEtHeure = new ArrayList<>();
+    /**
+     * Liste des cours et du nombre d'heures
+     */
     protected ArrayList <CoursEtHeure> listeCoursEtHeure = new ArrayList<>();
+    /**
+     * Liste des salles et du nombre d'heures
+     */
     protected ArrayList <SalleEtHeure> listeSalleEtHeure = new ArrayList<>();
 
-    public Classe(){
-
+    /**
+     * Constructeur paramétré de la classe
+     * @param sigle Sigle de la classe
+     * @param specialite Spécialité de la classe
+     * @param annee Année de la classe
+     * @param nbreEleves Le nombre d'élèves dans la classe
+     */
+    public Classe(String sigle, String specialite, int annee, int nbreEleves) {
+        this.sigle = sigle;
+        this.specialite = specialite;
+        this.annee = annee;
+        this.nbreEleves = nbreEleves;
     }
+
+    /**
+     * fonction verifiant si la liste des infos n'est pas vide
+     * @return Booléen vrai si la liste contient au moins une info, faux dans le cas contraire
+     */
     public boolean listeInfosVerif(){
         int longueur = listeInfos.size();
         if(longueur==0){
@@ -22,6 +64,9 @@ public class Classe {
         }
     }
 
+    /**
+     * fonction calculant le nombre d'heures total puis l'affiche.
+     */
     public void nbreHeuresTot(){
         int cptHeures=0;
         if(listeInfosVerif()){
@@ -31,9 +76,15 @@ public class Classe {
             System.out.println("Nombre d'heures total : "+cptHeures+".");
         }
     }
+
+    /**
+     * Fonction mettant à jour la liste d'enseigants et de nombre d'heures en parcourant la liste des informations.
+     * Si un enseignant existe déjà dans la liste d'enseignants et de nombre d'heures, la fonction ajoute le nombre d'heures associées
+     * à cet enseignant. Sinon, elle crée une nouvelle entrée pour l'enseignant avec le nombre d'heures spécifié.
+     */
     public void listeEnseignantsEtHeures(){
-        int cpt=1;
         EnseignantEtHeure ajout = new EnseignantEtHeure();
+        //verifier si la liste n'est pas vide
         if(listeInfosVerif()){
             for (Infos element : listeInfos){
                 ajout.setEnseignant(element.enseignant);
@@ -51,6 +102,11 @@ public class Classe {
             }
         }
     }
+    /**
+     * Fonction mettant à jour la liste des salles et de nombre d'heures en parcourant la liste des informations.
+     * Si une salle existe déjà dans la liste des salles et de nombre d'heures, la fonction ajoute le nombre d'heures associées
+     * à cette salle. Sinon, elle crée une nouvelle entrée pour la salle avec le nombre d'heures spécifié.
+     */
     public void listeSallesEtHeures(){
         SalleEtHeure ajout = new SalleEtHeure();
         if(listeInfosVerif()){
@@ -70,6 +126,11 @@ public class Classe {
             }
         }
     }
+    /**
+     * Fonction mettant à jour la liste des cours et de nombre d'heures en parcourant la liste des informations.
+     * Si un cours existe déjà dans la liste des cours et de nombre d'heures, la fonction ajoute le nombre d'heures associées
+     * à ce cours. Sinon, elle crée une nouvelle entrée pour le cours avec le nombre d'heures spécifié.
+     */
     public void listeCoursEtHeures(){
         CoursEtHeure ajout = new CoursEtHeure();
         if(listeInfosVerif()){
@@ -89,9 +150,23 @@ public class Classe {
             }
         }
     }
+
+    /**
+     * fonction permettant l'ajout d'un cours et de son nombre d'heures dans la liste des informations
+     * @param c cours à ajouter
+     * @param heures nombre d'heures du cours
+     */
     public void addCours(Cours c,int heures){
         listeInfos.add(new Infos(c,heures));
     }
+
+    /**
+     * Fonction permettant de modifier l'enseignant donnant un cours présant dans la liste des informations.
+     * La fonction parcourt la listeInfos et lorsqu'elle trouve le cours envoyé en paramètre,
+     * elle effectue la modification.
+     * @param c cours envoyé en paramètre afin de determiner l'emplacement de la modification
+     * @param e enseignant qui prendra la place de l'ancien
+     */
     public void modifCours(Cours c,Enseignant e){
         for (Infos element: listeInfos) {
             if(element.getCours().equals(c)){
@@ -100,6 +175,14 @@ public class Classe {
             }
         }
     }
+
+    /**
+     * Fonction permettant de modifier la salle présente dans la liste des informations.
+     * La fonction parcourt la listeInfos et lorsqu'elle trouve le cours envoyé en paramètre,
+     * elle effectue la modification.
+     * @param c cours envoyé en paramètre afin de determiner l'emplacement de la modification
+     * @param s salle qui prendra la place de l'anciene
+     */
     public void modifCours(Cours c,Salle s){
         for (Infos element: listeInfos) {
             if(element.getCours().equals(c)){
@@ -108,6 +191,14 @@ public class Classe {
             }
         }
     }
+
+    /**
+     * Fonction permettant de modifier le nombre d'heures présentes dans la liste des informations.
+     * La fonction parcourt la listeInfos et lorsqu'elle trouve le cours envoyé en paramètre,
+     * elle effectue la modification.
+     * @param c cours envoyé en paramètre afin de determiner l'emplacement de la modification
+     * @param heures heures qui prendront la place des anciennes
+     */
     public void modifCours(Cours c,int heures){
         for (Infos element: listeInfos) {
             if(element.getCours().equals(c)){
@@ -116,6 +207,12 @@ public class Classe {
             }
         }
     }
+
+    /**
+     * Fonction permettant de supprimer un element de la liste des informations à partir d'un cours envoyé en paramètre.
+     * La fonction parcourt la liste des informations afin de se placer où se trouve le cours, puis supprime l'information.
+     * @param c cours à supprimer
+     */
     public void suppCours(Cours c){
         listeInfos.removeIf(element -> (element.getCours()).equals(c));
         /*
@@ -132,8 +229,15 @@ public class Classe {
         * - methode forEach : permet d'afficher si la suppression est faite
         * */
     }
+
+    /**
+     * Fonction vérifiant si le nombre d'élève de la classe est inférieur ou égale a la capacité de cette dernière
+     * (envoyée en paramètre).
+     * @param salle salle de cours
+     * @return Booléen vrai si le nombre d'élèves est inférieur à la capacité de la classe et faux, si inverse.
+     */
     public boolean salleCapaciteOK(Salle salle){
-        return nbreEleves >= salle.getCapacite();
+        return nbreEleves <= salle.getCapacite();
     }
 }
 
